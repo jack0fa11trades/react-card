a727bc0dd79dc0a2bc334b51f9d1965a9a24dd65
Gaurab Upreti <gauravupreti1230@gmail.com>
2023-08-31 14:34:04 +0545

    added images

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import { useState } from "react";
-// import img1 from './images/frame1.png';
-// import img2 from "./images/frame2.png";
-// import img3 from "./images/frame3.png";
-// import img4 from "./images/frame4.png";
+import './App.css'
+import img1 from "./images/frame1.png";
+import img2 from "./images/frame2.png";
+import img3 from "./images/frame3.png";
+import img4 from "./images/frame4.png";
 
 function App() {
   const [cards] = useState([
     {
-      title: 'Card-1',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.'
+      title: "Card-1",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.",
+      image: img1,
     },
-
     {
-      title: 'Card-2',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.'
+      title: "Card-2",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.",
+      image: img2,
     },
-
     {
-      title: 'Card-3',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.'
+      title: "Card-3",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.",
+      image: img3,
     },
-
     {
-      title: 'Card-4',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.'
+      title: "Card-4",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.",
+      image: img4,
     },
+  ]);
 
-  ])
   return (
     <div>
       <section>
@@ -36,6 +38,7 @@ function App() {
             <div className="cards">
               {cards.map((card, i) => (
                 <div key={i} className="card">
+                  <img src={card.image} alt={`Image for ${card.title}`} />
                   <h3>{card.title}</h3>
                   <p>{card.text}</p>
                 </div>
